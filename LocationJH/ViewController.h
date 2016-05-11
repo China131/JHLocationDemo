6addedb1008cccbdd2752d4aec3a58b049a7d334
iOS163 <13162255392@163.com>
2016-05-11 18:27:08 +0800

    Initial Commit

---
+++ LocationJH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationJH
+//
+//  Created by cjatech-简豪 on 16/5/11.
+//  Copyright © 2016年 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationJH/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationJH
+//
+//  Created by cjatech-简豪 on 16/5/11.
+//  Copyright © 2016年 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
